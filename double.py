a032d16906fb2f0e12e6df2c1241012dc6332804
padams73 <pad@stac.school.nz>
2024-02-09 10:25:37 +1300

    Double it finished
    Added f-string to output
    Added if statement checking if number was positive

--- double.py
+++ double.py
@@ -1,12 +1,18 @@
 ''' This program takes a number as input and doubles it '''
 
+# Start a loop for error catching
 keep_asking = True
 while keep_asking:
     # Take number input as a float and then print double the number
     try:
-        number = float(input())
-        keep_asking = False
+        number = float(input("Enter a positive number: "))
+        if number >=0:
+            keep_asking = False
+        else:
+            print("Please enter a positive number")
+
     except ValueError:
         print("Please enter a number")
 
-print(number * 2)
+# Print the result
+print(f"{number} doubled is {number * 2}")
