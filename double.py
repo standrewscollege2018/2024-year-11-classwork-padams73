196c0cc3359aa0ca99e7dcbc289ed3e72af486f1
padams73 <pad@stac.school.nz>
2024-02-08 15:16:11 +1300

    Added a while loop to double it

--- double.py
+++ double.py
@@ -1,9 +1,12 @@
 ''' This program takes a number as input and doubles it '''
 
-# Take number input as a float and then print double the number
-try:
-    number = float(input())
-    print(number * 2)
-except ValueError:
-    print("Please enter a number")
+keep_asking = True
+while keep_asking:
+    # Take number input as a float and then print double the number
+    try:
+        number = float(input())
+        keep_asking = False
+    except ValueError:
+        print("Please enter a number")
 
+print(number * 2)
