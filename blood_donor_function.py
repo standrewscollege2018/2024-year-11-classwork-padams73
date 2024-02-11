12d5e14d34adf38da9168306e8c77a38e4c39552
padams73 <pad@stac.school.nz>
2024-02-12 09:40:54 +1300

    Blood donor task with a function
    Have also included constants

---
+++ blood_donor_function.py
@@ -0,0 +1,40 @@
+''' This program calculates whether a person is eligible to donate blood '''
+
+''' Check input is valid '''
+def check_input(n):
+    try:
+        n = int(n)
+        if n > 0:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+
+AGE_MIN = 16
+WEIGHT_MIN = 50
+
+# Get age
+get_age = True
+while get_age:
+    age = input("Enter your age: ")
+    if check_input(age):
+        get_age = False
+    else:
+        print("Enter positive integer")
+
+# Get weight
+get_weight = True
+while get_weight:
+    weight = input("Enter your weight: ")
+    if check_input(weight):
+        get_weight = False
+    else:
+        print("Enter positive integer")
+
+if int(age) >= AGE_MIN and int(weight) >= WEIGHT_MIN:
+    print("You are eligible")
+else:
+    print("You are not eligible")
+
+
---
+++ function_demo.py
@@ -0,0 +1,12 @@
+''' This program demonstrates how a function works '''
+
+''' multiply() multiplies two numbers and returns the result '''
+def multiply(a, b):
+    return a * b
+
+# Get two numbers from the user
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter second number:"))
+
+# Display the product of those two numbers
+print(multiply(number1, number2))
